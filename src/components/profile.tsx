4153de34dcc35df666ca9d882a15b3dbb9182791
lamia-x5 <lamia-abdullah@outlook.com>
2022-08-02 16:43:28 +0300

    fix the login and profile in css file

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -4,32 +4,31 @@ import { Link } from 'react-router-dom';
 
 export default function MyProfile() {
   return ( <><>
-      <h1 className="my-profile-h1">حسـابي</h1>
+      <h1 className="profile-super">حسـابي</h1>
       <div className="profile-container">
-          <div className="profile-in">
               <h4 className="profile-text">الاسم كامل *</h4>
-              <input className="profile-input" type="text" />
+              <input className="profile-input" type="text-profile" />
               <h4 className="profile-text">اسم العرض *</h4>
-              <input className="profile-input" type="name" />
+              <input className="profile-input" type="name-profile" />
               <h4 className="profile-text">البريد الإلكتروني * </h4>
-              <input className="profile-input" type="email" />
+              <input className="profile-input" type="email-profile" />
               <h4>تغيير كلمة المرور</h4>
               <h4 className="profile-text">كلمة المرور الحالية (اترك الحقل فارغاً إذا كنت لا تودّ تغييرها)</h4>
-              <input className="profile-input" type="password" />
+              <input className="profile-input" type="password-profile" />
               <h4 className="profile-text">كلمة المرور الجديدة (اترك الحقل فارغاً إذا كنت لا تودّ تغييرها)</h4>
-              <input className="profile-input" type="password" />
+              <input className="profile-input" type="password-profile" />
               <h4 className="profile-text">تأكيد كلمة المرور الجديدة</h4>
-              <input className="profile-input" type="password" />
+              <input className="profile-input" type="password-profile" />
               <div className="profile-flex">
-                  <label className="form-profile">
-                      <input type="checkbox" name="checkbox-profile" />
-                  </label>
-                  <p className="profile-text">اشترك في النشرة الإخبارية</p>
+              <label className="profile-checkbox">
+              <input type="checkbox" name="checkbox-profile" />
+                اشترك في النشرة الإخبارية
+               </label>
               </div>
               <div className="profile-btn">حفظ التغيرات</div>
-          </div>
-      </div>
-  </><div className="sidenav-account">
+          
+             </div>
+  </>       <div className="sidenav-account">
           <div className="icon-user"> <AiOutlineUser size={30} /></div>
           <p> lamia</p>
           <Link to={'/myOrdres'}><a href="g">طلباتي</a></Link>
